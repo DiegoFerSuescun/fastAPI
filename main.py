52743b91e0830507f46d506b9883b8073c9b517f
DiegoFerSuescun <dfsuesc@gmail.com>
2024-02-21 11:48:40 -0500

    Schema

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Body
 from fastapi.responses import HTMLResponse
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
 
 app= FastAPI()
@@ -9,11 +9,27 @@ app.version = "0.0.1"
 
 class Movie(BaseModel):
     id: Optional[int] = None
-    title: str
-    overview: str
-    year: int
-    rating: float
-    category: str
+    title: str = Field(min_length=5, max_length=15)
+    overview: str = Field(min_length=15, max_length=50)
+    year: int = Field(le=2022)
+    rating:float = Field(default=10, ge=1, le=10)
+    category:str = Field(default='Categoría', min_length=5, max_length=15)
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "id": 1,
+                    "title": "MiPelicula",
+                    "overview": "MiDescripcion",
+                    "year": 2022,
+                    "rating": 9.8,
+                    "category": "action"
+                }
+            ]
+        }
+    }
+        
     
 movies=[
     {
